1797c0e8bff11f3366dafd796a5e24343210f2b7
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-01-01 16:37:47 +0100

    update js

--- clicking-game.js
+++ clicking-game.js
@@ -1,2 +1,12 @@
+jQuery(function ($) {
 
+    var $button = $('#button');
+    $button.click(function () {
+        $button
+            .removeClass('btn-success')
+            .addClass('btn-danger')
+            .html('Click!!!');
+    });
+
+});
 
