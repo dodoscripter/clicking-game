8a88573c7bda9707557b55aa631797b643e97dd7
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-01-21 14:13:13 +0100

    add cheat detection

--- clicking-game.js
+++ clicking-game.js
@@ -2,7 +2,7 @@ jQuery(function ($) {
 
     var $button = $('#button');
     var $score = $('#score');
-    var game, score = 0;
+    var game, cheater, score = 0, last = 0;
     $button.click(function () {
         if (!game) {
             $button
@@ -11,12 +11,30 @@ jQuery(function ($) {
                 .html('Click!!!');
             $score.html('Score: 0');
             game = true;
+        } else if (cheater) {
+            $button
+                .removeClass('btn-danger')
+                .addClass('btn-danger')
+                .html('Game Over')
+                .attr({disabled:'disabled'});
+            $score.html('Cheater!!!');
+            score = 0;
+            last = 0;
+            // cheater = false;
+            // game = false;
         } else {
             score++;
             $score.html('Score: ' + score);
         }
         return false;
     });
+
+    setInterval(function () {
+        if (score - last > 50) {
+            cheater = true;
+        }
+        last = score;
+    }, 1000);
 
 });
 
